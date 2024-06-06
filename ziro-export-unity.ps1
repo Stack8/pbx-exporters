9d326b625bcfc5bf727de53cdce2741c4e9388be
amonzerol <antoine.monzerol@hotmail.com>
2024-06-06 16:12:34 -0400

    WIP create folder structure

--- ziro-export-unity.ps1
+++ ziro-export-unity.ps1
@@ -1,8 +1,32 @@
-$Credential = Get-Credential -Message "Insert Unity Username and Passsword"    
-$UnityHost = Read-Host "Please enter the Unity server URL"
+function Execute-GetOnUnity {
+    param (
+        [string]$UnityHost,
+        [string]$Endpoint,
+        [PSCredential]$Credential,
+        [string]$OutputFileName
+    )
 
-$headers = @{
-    "Accept" = "application/json"
+    $Url = $UnityHost + $Endpoint
+    $Headers = @{
+        "Accept" = "application/json"
+    }
+ 
+    $response = Invoke-RestMethod -Uri $Url -Headers $Headers -SkipCertificateCheck -Credential $Credential | ConvertTo-Json
+    $response | Out-File -FilePath $OutputFileName
 }
 
-$response = Invoke-WebRequest -Uri $UnityHost -Headers $headers -SkipCertificateCheck -Credential $Credential
+$Credential = Get-Credential -Message "Insert Unity Username and Password"    
+$UnityHost = Read-Host "Please enter the Unity server URL (ex: https://myunity.com/)"
+
+New-Item -Name "users" -ItemType "directory" -Force
+New-Item -Name "callhandlers" -ItemType "directory" -Force
+New-Item -Name "distributionlists" -ItemType "directory" -Force
+New-Item -Name "directoryhandlers" -ItemType "directory" -Force
+New-Item -Name "interviewhandlers" -ItemType "directory" -Force
+New-Item -Name "routingrules" -ItemType "directory" -Force
+New-Item -Name "partitions" -ItemType "directory" -Force
+New-Item -Name "schedules" -ItemType "directory" -Force
+New-Item -Name "schedulesets" -ItemType "directory" -Force
+
+Execute-GetOnUnity $UnityHost 'vmrest/users/' $Credential 'users/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/handlers/callhandlers' $Credential 'callhandlers/list.json'
