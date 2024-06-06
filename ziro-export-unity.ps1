f2a148b07a7e048c8da76a631e22b5819b4958f3
amonzerol <antoine.monzerol@hotmail.com>
2024-06-06 16:17:26 -0400

    WIP get data from unity

--- ziro-export-unity.ps1
+++ ziro-export-unity.ps1
@@ -18,15 +18,22 @@ function Execute-GetOnUnity {
 $Credential = Get-Credential -Message "Insert Unity Username and Password"    
 $UnityHost = Read-Host "Please enter the Unity server URL (ex: https://myunity.com/)"
 
-New-Item -Name "users" -ItemType "directory" -Force
-New-Item -Name "callhandlers" -ItemType "directory" -Force
-New-Item -Name "distributionlists" -ItemType "directory" -Force
-New-Item -Name "directoryhandlers" -ItemType "directory" -Force
-New-Item -Name "interviewhandlers" -ItemType "directory" -Force
-New-Item -Name "routingrules" -ItemType "directory" -Force
-New-Item -Name "partitions" -ItemType "directory" -Force
-New-Item -Name "schedules" -ItemType "directory" -Force
-New-Item -Name "schedulesets" -ItemType "directory" -Force
+New-Item -Name "users" -ItemType Directory -Force
+New-Item -Name "callhandlers" -ItemType Directory -Force
+New-Item -Name "distributionlists" -ItemType Directory -Force
+New-Item -Name "directoryhandlers" -ItemType Directory -Force
+New-Item -Name "interviewhandlers" -ItemType Directory -Force
+New-Item -Name "routingrules" -ItemType Directory -Force
+New-Item -Name "partitions" -ItemType Directory -Force
+New-Item -Name "schedules" -ItemType Directory -Force
+New-Item -Name "schedulesets" -ItemType Directory -Force
 
 Execute-GetOnUnity $UnityHost 'vmrest/users/' $Credential 'users/list.json'
 Execute-GetOnUnity $UnityHost 'vmrest/handlers/callhandlers' $Credential 'callhandlers/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/distributionlists' $Credential 'distributionlists/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/handlers/directoryhandlers' $Credential 'directoryhandlers/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/handlers/interviewhandlers' $Credential 'interviewhandlers/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/routingrules' $Credential 'routingrules/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/partitions' $Credential 'partitions/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/schedules' $Credential 'schedules/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/schedulesets' $Credential 'schedulesets/list.json'
