046bdf2f00a1760c63632e5d474a3ec6ad0f309e
amonzerol <antoine.monzerol@hotmail.com>
2024-06-10 17:06:09 -0400

    Implement paging

--- ziro-export-unity.ps1
+++ ziro-export-unity.ps1
@@ -3,7 +3,8 @@ function Execute-GetOnUnity {
         [string]$UnityHost,
         [string]$Endpoint,
         [PSCredential]$Credential,
-        [string]$OutputFileName
+        [string]$OutputFileName,
+        [string]$ResourceName
     )
 
     $PageNumber = 1
@@ -15,9 +16,24 @@ function Execute-GetOnUnity {
         "Accept" = "application/json"
     }
 
+    $ResourcesArray = @()
+
  
-    $response = Invoke-RestMethod -Uri $Url -Headers $Headers -SkipCertificateCheck -Credential $Credential | ConvertTo-Json
-    $response | Out-File -FilePath $OutputFilePath
+    $Response = Invoke-RestMethod -Uri $Url -Headers $Headers -SkipCertificateCheck -Credential $Credential
+    $Resources = $Response.$ResourceName
+    $TotalResources = [int]$Response."@total"
+    $ResourcesArray += $Resources
+    
+    while ($ResourcesArray.Count -lt $TotalResources) {
+        $PageNumber++
+        $Url = $UnityHost + $Endpoint + "?rowsPerPage=1&pageNumber=" + $PageNumber
+        $Response = Invoke-RestMethod -Uri $Url -Headers $Headers -SkipCertificateCheck -Credential $Credential
+        $Resources = $Response.$ResourceName
+        $ResourcesArray += $Resources
+    }
+
+    $JsonOutput = ConvertTo-Json $ResourcesArray
+    $JsonOutput | Out-File -FilePath $OutputFilePath
 }
 
 $Credential = Get-Credential -Message "Insert Unity Username and Password"    
@@ -34,15 +50,15 @@ New-Item -Name "output-unity/partitions" -ItemType Directory -Force
 New-Item -Name "output-unity/schedules" -ItemType Directory -Force
 New-Item -Name "output-unity/schedulesets" -ItemType Directory -Force
 
-Execute-GetOnUnity $UnityHost 'vmrest/users/' $Credential 'users/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/handlers/callhandlers' $Credential 'callhandlers/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/distributionlists' $Credential 'distributionlists/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/handlers/directoryhandlers' $Credential 'directoryhandlers/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/handlers/interviewhandlers' $Credential 'interviewhandlers/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/routingrules' $Credential 'routingrules/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/partitions' $Credential 'partitions/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/schedules' $Credential 'schedules/list.json'
-Execute-GetOnUnity $UnityHost 'vmrest/schedulesets' $Credential 'schedulesets/list.json'
+Execute-GetOnUnity $UnityHost 'vmrest/users/' $Credential 'users/list.json' 'User'
+Execute-GetOnUnity $UnityHost 'vmrest/handlers/callhandlers' $Credential 'callhandlers/list.json' 'CallHandler'
+Execute-GetOnUnity $UnityHost 'vmrest/distributionlists' $Credential 'distributionlists/list.json' 'DistributionList'
+Execute-GetOnUnity $UnityHost 'vmrest/handlers/directoryhandlers' $Credential 'directoryhandlers/list.json' 'DirectoryHandler'
+Execute-GetOnUnity $UnityHost 'vmrest/handlers/interviewhandlers' $Credential 'interviewhandlers/list.json' 'InterviewHandler'
+Execute-GetOnUnity $UnityHost 'vmrest/routingrules' $Credential 'routingrules/list.json' 'RoutingRule'
+Execute-GetOnUnity $UnityHost 'vmrest/partitions' $Credential 'partitions/list.json' 'Partition'
+Execute-GetOnUnity $UnityHost 'vmrest/schedules' $Credential 'schedules/list.json' 'Schedule'
+Execute-GetOnUnity $UnityHost 'vmrest/schedulesets' $Credential 'schedulesets/list.json' 'ScheduleSet'
 
 Compress-Archive -Path output-unity/* -DestinationPath output-unity.zip -Force
 Remove-Item -Path output-unity -Recurse
