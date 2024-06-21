cb516a99c793dee6c0e2ff92db4034c06ac9c713
Philippe Min <min.philippe@gmail.com>
2024-06-21 00:56:49 -0400

    Use #Requires for PS version validation

--- ziro-export-uccx.ps1
+++ ziro-export-uccx.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 7.0
+
 class UccxConnector {
     [string] $BaseUrl
     [PSCredential] $Credential
@@ -200,13 +202,6 @@ function Export-AreaCodes([UccxConnector]$Connector, $ExportDirectory) {
     return Export-UccxEntities @parameters
 }
 
-
-# Check powershell version requirement
-$minimumPowershellVersion = [System.Management.Automation.SemanticVersion]::new(7, 0, 0)
-if ($PSVersionTable.PSVersion -lt $minimumPowershellVersion) {
-    Write-Error "This script requires Powershell version ${minimumPowershellVersion} or greater."
-    exit 1
-}
 
 $serverUrl = Read-Host 'UCCX server URL (https://myuccx.company.com)'
 $credential = Get-Credential -Message 'Enter username and password'
